8f734b549d710982e6db3ec8e29f6e645cd0639c
Abhishek Gupta <abhi7310456570@gmail.com>
2026-02-14 12:00:56 +0530

    Add files via upload

---
+++ sumof_50num.py
@@ -0,0 +1,13 @@
+
+
+#...................
+#......................
+#number from 1 to 50 by using for loop
+print("sum of intgers number from 1 to 50 by using for loop ")
+
+total = 0
+
+for i in range(1, 51):
+    total += i
+
+print("Sum from 1 to 50:", total)
